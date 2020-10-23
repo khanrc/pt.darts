022e72dc2df439b47314042e87419c5c788d8fd9
Matt <matthew.poyser@durham.ac.uk>
2020-10-23 12:41:55 +0100

    dynamic dataset

--- config.py
+++ config.py
@@ -14,7 +14,6 @@ def get_parser(name):
     # print default value always
     parser.add_argument = partial(parser.add_argument, help=' ')
     # idx = sys.argv.index('--name')
-    raise AttributeError(sys.argv)
     return parser
 
 

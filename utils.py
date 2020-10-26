e61c45bacf54b5b5619b46da88d1c349d2d69a10
Matt <matthew.poyser@durham.ac.uk>
2020-10-26 15:08:20 +0000

    debug dynamic darts

--- utils.py
+++ utils.py
@@ -69,7 +69,7 @@ def get_data(dataset, data_path, cutout_length, validation):
         trn_data = dset_cls(root=data_path, train=True, download=True, transform=trn_transform)
 
         # assuming shape is NHW or NHWC
-        shape = trn_data.train_data.shape
+        shape = trn_data.data.shape
         input_channels = 3 if len(shape) == 4 else 1
         assert shape[1] == shape[2], "not expected shape = {}".format(shape)
         input_size = shape[1]

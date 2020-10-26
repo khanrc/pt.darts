ef6d4d968095ac1cdd788b2cc02fa6bd00616920
Matt <matthew.poyser@durham.ac.uk>
2020-10-26 14:12:05 +0000

    debug dynamic darts

--- search.py
+++ search.py
@@ -137,6 +137,7 @@ def train(train_loader, valid_loader, model, architect, w_optim, alpha_optim, lr
 
     hardness = [None for i in range(len(train_loader))]
     for step, ((trn_X, trn_y), (val_X, val_y)) in enumerate(zip(train_loader, valid_loader)):
+        raise AttributeError(len(trn_X), len(train_loader))
         trn_X, trn_y = trn_X.to(device, non_blocking=True), trn_y.to(device, non_blocking=True)
         val_X, val_y = val_X.to(device, non_blocking=True), val_y.to(device, non_blocking=True)
         N = trn_X.size(0)

cf48ae148b75bce7f1d2563dffc470261453d2f8
Matt <matthew.poyser@durham.ac.uk>
2020-10-29 12:43:30 +0000

    debug for cifar

--- utils.py
+++ utils.py
@@ -67,7 +67,7 @@ def get_data(dataset, data_path, cutout_length, validation):
             grayscale=grayscale,
             isTsne=True)
         input_size = len(trn_data)
-        raise AttributeError(trn_data.data.cur_set[0].shape)
+        raise AttributeError(trn_data.dataset.cur_set[0].shape)
     else:
         trn_data = dset_cls(root=data_path, train=True, download=False, transform=trn_transform)
 

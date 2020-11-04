2237fafd4bd53f6bf79c0b564651f9be2cb0788e
Matt <matthew.poyser@durham.ac.uk>
2020-11-04 12:39:59 +0000

    temp remove plot

--- search.py
+++ search.py
@@ -106,8 +106,8 @@ def main():
             # genotype as a image
             plot_path = os.path.join(config.plot_path, "EP{:02d}".format(epoch+1))
             caption = "Epoch {}".format(epoch+1)
-            plot(genotype.normal, plot_path + "-normal", caption)
-            plot(genotype.reduce, plot_path + "-reduce", caption)
+            # plot(genotype.normal, plot_path + "-normal", caption)
+            # plot(genotype.reduce, plot_path + "-reduce", caption)
 
             # save
             if best_top1 < top1:

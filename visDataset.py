4992b2d08fd82fce1c2dc54175e607d5c819d15f
Matt <matthew.poyser@durham.ac.uk>
2020-12-04 12:15:34 +0000

    visualize bars

--- visDataset.py
+++ visDataset.py
@@ -11,6 +11,8 @@ def main():
             data = f.readlines()
             train_accs = []
             val_accs = []
+            bars = []
+            count = 0
             for datum in data:
                 try:
                     bar_idx = str(datum).index("|")
@@ -25,14 +27,20 @@ def main():
                         val_accs.append(acc)
                     else:
                         print("neither")
+                    count += 1
                 except ValueError:
-                    pass
+                    bars.append(count)
 
             assert len(train_accs) == len(val_accs), "len of train and val accuracies should be same"
             x_axis = [i for i in range(len(train_accs))]
             fig, ax = plt.subplots()
             ax.plot(x_axis, train_accs, color="green")
             ax.plot(x_axis, val_accs, color="red")
+
+            xposition = [0.3, 0.4, 0.45]
+            for xc in xposition:
+                plt.axvline(x=xc, color='k', linestyle='--')
+                
             fig.savefig("test.png")
             print(train_accs, val_accs)
 

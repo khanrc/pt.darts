1b68059c051dc573d6947b9cfa0b8c8825606623
Matt <matthew.poyser@durham.ac.uk>
2020-12-04 12:05:56 +0000

    init commit

--- visDataset.py
+++ visDataset.py
@@ -2,20 +2,29 @@ import os
 
 
 def main():
-    path_name = "/home2/lgfm95/nas/darts/accuracies.out"
+    path_name = "/home2/lgfm95/nas/darts/accuraciesFashion.out"
     if os.path.isfile(path_name):
         with open(path_name, "r") as f:
             data = f.readlines()
-            accs = []
+            train_accs = []
+            val_accs = []
             for datum in data:
-                bar_idx = str(datum).index("|")
-                suffix = datum[bar_idx:]
-                spl = suffix.split(" ")
-                tv = spl[1]
-                acc = spl[-1]
-                acc = float(acc.replace("%\n", ""))
-                accs.append([tv,acc])
-            print(accs)
+                try:
+                    bar_idx = str(datum).index("|")
+                    suffix = datum[bar_idx:]
+                    spl = suffix.split(" ")
+                    tv = spl[1]
+                    acc = spl[-1]
+                    acc = float(acc.replace("%\n", ""))
+                    if "Train:" in tv:
+                        train_accs.append(acc)
+                    elif "Valid:" in tv:
+                        val_accs.append(acc)
+                    else:
+                        print("neither")
+                except AttributeError:
+                    pass
+            print(train_accs, val_accs)
 
 
 if __name__ == "__main__":

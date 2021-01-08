bdb121c371fcb476b23bfea4b80eb6be46431574
Matt <matthew.poyser@durham.ac.uk>
2021-01-08 10:30:40 +0000

    debug mastery

--- search.py
+++ search.py
@@ -303,7 +303,7 @@ def get_mastered(hardness):
     print("len hard ones", np.where(np.array(hardness) > 0.5))
     print("len hard ones", len(np.where(np.array(hardness) > 0.5)[0]))
     print("hardness calculations: ", (len(np.where(np.array(hardness) > config.hardness)[0]) / len(hardness)), config.mastery)
-    if (len(np.where(np.array(hardness) > config.hardness)) / len(hardness)) < config.mastery:
+    if (len(np.where(np.array(hardness) > config.hardness)[0]) / len(hardness)) < config.mastery:
         print("therefore not mastered")
         return 0
     # if len(np.where(np.array(hardness) < config.mastery)) > len(hardness)-2:

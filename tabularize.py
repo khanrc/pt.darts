febf8676919380558b8c156d7278e787d0c71976
Matt <matthew.poyser@durham.ac.uk>
2021-01-19 10:49:01 +0000

    init commit

---
+++ tabularize.py
@@ -0,0 +1,17 @@
+import os
+import re
+
+
+def main():
+    if os.path.exists("./totab.out"):
+        fp = open("./totab.out")
+        lines = fp.readlines()
+        for line in lines:
+            row = line.split(" ")
+            headers = row[0].split("aa")
+            for header in headers:
+                print(header)
+
+
+if __name__ == '__main__':
+    main()

d28d05470faa91919bc9bda4a7896ad143d68430
Matt <matthew.poyser@durham.ac.uk>
2021-02-22 11:50:16 +0000

    ready for subset reduced size on vanilla

--- utils.py
+++ utils.py
@@ -93,11 +93,13 @@ def get_data(dataset, data_path, cutout_length, validation, search):
             trn_data = SubDataset(transforms=val_transform, val=False, dataset_name="planes", subset_size=100)
             # trn_data = dset_cls(root=data_path, train=True, download=False, transform=trn_transform)
 
-        # assuming shape is NHW or NHWC
-        shape = trn_data.data.shape
-        input_channels = 3 if len(shape) == 4 else 1
-        assert shape[1] == shape[2], "not expected shape = {}".format(shape)
-        input_size = shape[1]
+        input_size = len(trn_data)
+        input_channels = 3 if len(trn_data.cur_set[0].getbands()) == 3 else 1 # getbands() gives rgb if rgb, l if grayscale
+        # # assuming shape is NHW or NHWC
+        # shape = trn_data.data.shape
+        # input_channels = 3 if len(shape) == 4 else 1
+        # assert shape[1] == shape[2], "not expected shape = {}".format(shape)
+        # input_size = shape[1]
 
     ret = [input_size, input_channels, n_classes, trn_data]
     if validation: # append validation data

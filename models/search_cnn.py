60aefe0114f1a3258b59cc681e413203e6dc06e6
Matt <matthew.poyser@durham.ac.uk>
2021-03-12 11:22:59 +0000

    debug cityscapes

--- models/search_cnn.py
+++ models/search_cnn.py
@@ -126,9 +126,12 @@ class SearchCNNController(nn.Module):
         return nn.parallel.gather(outputs, self.device_ids[0])
 
     def loss(self, X, y):
-        raise AttributeError(y.shape)
         logits = self.forward(X)
-        return self.criterion(logits, y)
+        try:
+            return self.criterion(logits, y)
+        except RuntimeError as e:
+            print(e)
+            raise AttributeError(y.shape, logits.shape)
 
     def print_alphas(self, logger):
         # remove formats

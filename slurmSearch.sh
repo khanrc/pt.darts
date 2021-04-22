de06b86bcf347d15e6bf807076b05774ed3dbdc3
Matt <matthew.poyser@durham.ac.uk>
2021-04-22 12:32:49 +0100

    correct autoencoder

--- slurmSearch.sh
+++ slurmSearch.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -c 4
+<<<<<<< HEAD
+##SBATCH -p gpu-private
+#SBATCH -p res-gpu-small
+#SBATCH --qos=short
+#SBATCH --job-name=darts
+#SBATCH --gres=gpu:1
+#SBATCH -o search.out
+##SBATCH --mem=72g
+#SBATCH --mem=28g
+=======
 #SBATCH -p gpu-private
 ##SBATCH -p res-gpu-small
 #SBATCH --qos=short
@@ -9,12 +19,15 @@
 #SBATCH -o outputSearch.out
 #SBATCH --mem=72g
 ##SBATCH --mem=28g
+>>>>>>> 2e5db1ba64a3a015b5cf47d23bec6dd1e4aff871
 #SBATCH -t 2-0:0:0
 
 #module unload cuda
 #module load cuda/9.0
 #source venv/bin/activate
 
+<<<<<<< HEAD
+=======
 #python3 augment.py --name cifar10-mg --dataset cifar10 --gpus 0 \
 #    --batch_size 32 --workers 8 --print_freq 50 --lr 0.1 \
 #    --genotype "Genotype(
@@ -24,5 +37,6 @@
 #    reduce_concat=range(2, 6)
 #)"
 
+>>>>>>> 2e5db1ba64a3a015b5cf47d23bec6dd1e4aff871
 bash bashSearch.sh
 

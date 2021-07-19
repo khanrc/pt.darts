72b9e2024ec21235d9b69acd1a449b186d055aca
Matt <matthew.poyser@durham.ac.uk>
2021-07-19 16:37:38 +0100

    checkpoint saving

--- config.py
+++ config.py
@@ -75,6 +75,7 @@ class SearchConfig(BaseConfig):
         parser.add_argument('--hardness', default=0.5, type=float, help="hardness threshold")
         parser.add_argument('--visualize', default=False, type=bool, help="are we visualizing?")
         parser.add_argument('--bede', default=False, type=bool, help="are we using bede server?")
+        parser.add_argument('--resume', default=None, type=str, help="where to save pth file")
 
         return parser
 

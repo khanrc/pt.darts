8924738a26c574ca944f1dbf706aebd2a8f97b24
Matt <matthew.poyser@durham.ac.uk>
2021-11-23 11:25:35 +0000

    debug obj van

--- utils.py
+++ utils.py
@@ -139,7 +139,7 @@ def get_data(dataset, data_path, cutout_length, validation, search, bede):
         input_channels = 3 if len(trn_data.bands) == 3 else 1 # getbands() gives rgb if rgb, l if grayscale
     else:
         if config.vanilla:
-            if dataset == 'imagenet':
+            if dataset == 'imagenet' or dataset == "imageobj":
                 trn_data = SubDataset(transforms=trn_transform, val_transforms=val_transform, val=False,
                                       dataset_name=dynamic_name)
                 input_size = len(trn_data)

c9c69759d0306c2e5e5a7cd72390a1843eba7aad
Matt <matthew.poyser@durham.ac.uk>
2022-01-31 12:26:30 +0000

    pure det validation and consistency

--- det_dataset.py
+++ det_dataset.py
@@ -41,7 +41,7 @@ class Imagenet_Det:
 
                 bbox = object.find("bndbox")
                 x1, x2, y1, y2 = bbox.find("xmin").text, bbox.find("xmax").text, bbox.find("ymin").text, bbox.find("ymax").text
-                fine.append([x1,y1,x2,y2])
+                fine.append([self.classes[class_id], x1,y1,x2,y2])
 
             if len(fine) > 0:
                 self.fine.append(fine)
@@ -56,12 +56,16 @@ class Imagenet_Det:
 
         print(f"have only seen {len(self.classes.keys())} keys")
 
+
+        self.data = [f"{self.image_path}/{a_data}.JPEG" for a_data in self.data]
+        print(self.fine[0])
+
     def __len__(self):
         assert len(self.data) == len(self.fine)
         return len(self.data)
 
     def __getitem__(self, item):
-        with Image.open(f"{self.image_path}/{self.data[item]}.JPEG") as image:
+        with Image.open(self.data[item]) as image:
             if image.mode != 'RGB':
                 image = image.convert('RGB')
             if self.transforms is not None:
@@ -82,9 +86,10 @@ if __name__ == "__main__":
         mean=[0.13066051707548254],
         std=[0.30810780244715075])
     perc_transforms = tf.Compose([
-        tf.RandomResizedCrop((224,224)),
+        tf.Resize((224,224)),
+        tf.RandomAffine(degrees=15, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=0.1),
         tf.ToTensor(),
-        normalize,
+        # normalize
     ])
 
     dset = Imagenet_Det(root="/hdd/PhD/data/imagenet2017detection", transforms=perc_transforms)
@@ -100,30 +105,22 @@ if __name__ == "__main__":
                                                     num_workers=0,
                                                     pin_memory=False)
 
-    idx_per_label = [None for i in range(184)]
-    fine = []
-    idx = [i for i in range(100)]
-    print(idx)
 
-    for q, (_, lab) in enumerate(split_loader):
-        fine.append(lab)
-        if q % 100 == 0:
-            print(q)
-
-    cur_labels = [fine[idx] for idx in idx]
-
-    print(cur_labels[0])
-    # for q, (im, label) in enumerate(split_loader):
-    #     assert len(label) == 184
-    #     raise AttributeError(type(label), len(label))
-    #     for j in range(len(label)):  # 184
-    #         lab = label[j]
-    #         if lab == 1:
-    #             if idx_per_label[j] is None:
-    #                 idx_per_label[j] = [q]
-    #             else:
-    #                 idx_per_label[j].append(q)
-    #     if q%100 == 0:
-    #         print(q)
-    # for i in range(184):
-    #     print(i, len(idx_per_label[i]))
+    classes = dset.classes
+    inv_map = {v: k for k, v in classes.items()}
+    for i, (image, labs) in enumerate(split_loader):
+        if i >= 50:
+            break
+        # os.makedirs(f"/data/mining/objval/dataloader/{i}", exist_ok=True)
+        os.makedirs(f"/hdd/PhD/nas/pt.darts/tempSave/puredetval/{i}", exist_ok=True)
+        pil_image = tf.ToPILImage()(image[0])
+        copy_image = pil_image.copy()
+        # copy_image.save(f"/data/mining/objval/dataloader/{i}/0.png")
+        copy_image.save(f"/hdd/PhD/nas/pt.darts/tempSave/puredetval/{i}/0.png")
+        # with open(f"/data/mining/objval/dataloader/{i}/0.txt", "w") as new_f:
+        with open(f"/hdd/PhD/nas/pt.darts/tempSave/puredetval/{i}/0.txt", "w") as new_f:
+            for lab in labs:
+                lab_class = lab[0]
+                # raise AttributeError(inv_map[lab_class], lab_class)
+                new_f.write(inv_map[lab_class.item()])
+                new_f.write("\n")

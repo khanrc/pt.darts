157c94b5155906da4d1d37cdf1b0fac8917cadc5
Matt <matthew.poyser@durham.ac.uk>
2022-02-08 11:38:52 +0000

    backward architect

--- architect.py
+++ architect.py
@@ -64,8 +64,7 @@ class Architect():
         # compute gradient
         v_alphas = tuple(self.v_net.alphas())
         v_weights = tuple(self.v_net.weights())
-        # v_grads = torch.autograd.grad(loss, v_alphas + v_weights)
-        v_grads = torch.autograd.grad(loss, self.v_net.weights())
+        v_grads = torch.autograd.grad(loss, v_alphas + v_weights)
         dalpha = v_grads[:len(v_alphas)]
         dw = v_grads[len(v_alphas):]
 

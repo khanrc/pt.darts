7396ee7b86fbad947d8bcef61b08d8724c9fc0da
Matt <matthew.poyser@durham.ac.uk>
2022-02-15 14:41:42 +0000

    fix rpn input within darts

--- models/search_cnn_obj.py
+++ models/search_cnn_obj.py
@@ -72,7 +72,7 @@ class SearchCNN(nn.Module):
         reduction_p = False
         for i in range(n_layers):
             # Reduce featuremap size and double channels in 1/3 and 2/3 layer.
-            if i in [n_layers//3]:#, 2*n_layers//3]:
+            if i in [n_layers//3, 2*n_layers//3]:
                 C_cur *= 2
                 reduction = True
             else:

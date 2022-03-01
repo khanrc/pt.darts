d07771347d7a99ae5be14d761c94c979b9bc880e
Matt <matthew.poyser@durham.ac.uk>
2022-03-01 22:37:48 +0000

    correct autoencoder path

--- utils.py
+++ utils.py
@@ -100,7 +100,7 @@ def get_data(dataset, data_path, cutout_length, validation, search, bede, is_con
         n_classes = 91
         dynamic_name = "coco_det"
         grayscale = False
-        auto_resume = "/home2/lgfm95/hem/perceptual/ganPercMaskGood.pth.tar"
+        auto_resume = "/home2/lgfm95/hem/perceptual/ganPercObjGood.pth.tar"
         is_detection = True
         convert_to_paths = False
         # convert_to_lbl_paths = True

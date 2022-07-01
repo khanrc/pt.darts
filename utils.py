dcff995d02a7bf27385115a92f08cc306d3c307f
Matt <matthew.poyser@durham.ac.uk>
2022-07-01 13:12:42 +0100

    cifar100 ready

--- utils.py
+++ utils.py
@@ -194,7 +194,7 @@ def get_data(dataset, data_path, cutout_length, validation, search, bede, is_con
                 input_size = len(trn_data)
                 input_channels = 3
             else:
-                trn_data = dset_cls(root=data_path, train=True, download=True, transform=trn_transform)        # # assuming shape is NHW or NHWC
+                trn_data = dset_cls(root=data_path, train=True, download=False, transform=trn_transform)        # # assuming shape is NHW or NHWC
                 shape = trn_data.data.shape
                 input_channels = 3 if len(shape) == 4 else 1
                 assert shape[1] == shape[2], "not expected shape = {}".format(shape)

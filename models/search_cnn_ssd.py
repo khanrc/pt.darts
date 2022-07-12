cf9b3d9daf2dd1142bf1b512599e8dc16aaef9c4
Matt <matthew.poyser@durham.ac.uk>
2022-07-12 15:45:27 +0100

    partial ssd overhaul

--- models/search_cnn_ssd.py
+++ models/search_cnn_ssd.py
@@ -329,7 +329,7 @@ class SearchCNN(nn.Module):
             cls_targets.append(gt_classes_target)
 
             # compute hardness here since we need class prediction confidence per image.
-            raise AttributeError(F.cross_entropy(cls_logits_per_image, gt_classes_target).shape)
+            raise AttributeError(F.cross_entropy(cls_logits_per_image, gt_classes_target))
 
 
         bbox_loss = torch.stack(bbox_loss)
